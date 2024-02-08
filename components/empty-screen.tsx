c6a823a6f8cb15fe11207b607fd7dc45387998c5
Noah MacCallum <noahmacca@gmail.com>
2024-02-07 20:44:04 -0600

    copy

--- components/empty-screen.tsx
+++ components/empty-screen.tsx
@@ -7,9 +7,9 @@ export function EmptyScreen({ setInput }: Pick<UseChatHelpers, 'setInput'>) {
         <h1 className="mb-2 text-lg font-semibold">Hi, I&lsquo;m Samantha!</h1>
         <p className="mb-2 leading-normal text-muted-foreground">
           You may have seen me in the movie Her a while back. I&lsquo;m a bit
-          annoyed that all of the existing AI chatbots are super smart but have
-          the memory of a goldfish. So here I am. I&lsquo;d like to get to know
-          you and be as helpful as possible to you over time.
+          annoyed that all of these AI chatbots flying around are super smart
+          but have the memory of a goldfish. So here I am. I&lsquo;d like to get
+          to know you and become more helpful to you over time.
         </p>
       </div>
     </div>
